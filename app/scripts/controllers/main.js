7600429ecdce9ae0a337d32ef2e0805f61ef871c
Avila Salvador A <salvador.avila@thermofisher.com>
2017-03-07 01:28:58 -0800

    Updated classes with tests

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,28 +11,87 @@ angular.module('tddApp')
 .controller('MainCtrl', ['$scope',
   function ($scope) {
     // Init $scope variables
-    $scope.layoutState = 'initial';
+    $scope.buttonText = 'Show';
     $scope.showImage = false;
-
-    // Write a function that :
-    // - Receives 2 integer numbers : a and b
-    // - Return a boolean value
-
-    // Function : sum()
-    $scope.sum = function(a,b){
-    }
+    $scope.items = [];
+    $scope.itemsFormatted = false;
 
     // Function: hideImage()
     $scope.hideImage = function(){
       $scope.showImage = !$scope.showImage;
-      $scope.layoutState = 'altered';
+
+      // Update button text
+      if($scope.showImage)
+        $scope.buttonText = 'Hide';
+      else {
+        $scope.buttonText = 'Show';
+      }
     }
 
+
+    // Write a function that :
+    // - Receives 2 integer numbers : a and b
+    // - Return a boolean value
+    // - Return true if : a-b = 0
+    // - Return false if : a-b != 0
+
+    // Function : sum()
+    $scope.sum = function(a,b){
+
+      // 1)
+      // var result = a - b;
+      // if(a === b)
+      //   return true;
+      // else {
+      //   return false;
+      // }
+
+      // 2)
+      // return result === 0;
+
+      // 3)
+      //return true;
+
+      // 4)
+      return a === b;
+    }
+
+
+
     // Foreach, Called, Called times ()
+    $scope.populate = function() {
 
+      $scope.items = [
+        {
+          'name' : 'Orange',
+          'username':'Apple',
+          'email' : 'Banana'
+        },
+        {
+          'name' : 'Orange',
+          'username':'Apple',
+          'email' : 'Banana'
+        },
+        {
+          'name' : 'Orange',
+          'username':'Apple',
+          'email' : 'Banana'
+        }
+      ];
 
-    // Promise resolution
+      $scope.items.forEach(function(item){
+        //item.email = "something@gmail.com";
 
+        $scope.format(item);
+      });
+
+      // $scope.format($scope.items);
+
+    }
+
+    $scope.format = function(item){
+      item.email = "something@gmail.com";
+    };
 
 
     // Results or behavior?
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,10 +1,8 @@
 'use strict';
 
 describe('Controller: MainCtrl', function () {
-
   // load the controller's module
   beforeEach(module('tddApp'));
-
   var MainCtrl,
     scope;
 
@@ -17,29 +15,107 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
+  // Variables
   describe('Initial Values', function(){
     it('Check initial value of \'showImage\' flag', function(){
       expect(scope.showImage).toBeFalsy();
     });
 
-    it('Check initial value of \'layoutState\' variable', function(){
-      expect(scope.layoutState).toBe('initial');
+    it('Check initial value of \'buttonText\' variable', function(){
+      expect(scope.buttonText).toBe('Show');
     });
   });
 
-
+  // Image
   describe('hideImage', function(){
 
-    // it('sets the value of \'showImage\' to TRUE', function(){
-      // expect(scope.showImage).toBeFalsy();
-    // });
+    // beforeEach(inject(function () {
+    //   scope.hideImage();
+    // }));
 
-    // it('sets the value of \'showImage\' to TRUE', function(){
-    // })
+    it('Sets the value of \'showImage\' to TRUE', function(){
+      scope.hideImage();
+      expect(scope.showImage).toBeTruthy();
+    });
+
+    it('sets the value of \'buttonText\' variable', function(){
+      scope.hideImage();
+      expect(scope.buttonText).toBe('Hide');
+    });
+
+    it('sets the value of \'buttonText\' variable', function(){
+      scope.hideImage();
+      scope.hideImage();
+      expect(scope.buttonText).toBe('Show');
+    });
+  });
+
+  // Sum
+  describe('sum', function(){
+    it('Function exists and receives 2 arguments as input', function(){
+      var result = scope.sum(4,4);
+      expect(result).toBeTruthy();
+    });
+
+    /*
+    it('If input numbers are negative numbers', function(){
+      var result = scope.sum(-2,-2);
+      expect(result).toBeTruthy();
+    });
+
+    it('If input numbers are positive numbers', function(){
+      var result = scope.sum(2,2);
+      expect(result).toBeTruthy();
+    });
+
+    it('If input numbers are positive and negative numbers', function(){
+      var result = scope.sum(2,-2);
+      expect(result).toBeFalsy();
+    });
+    */
+
+    it('If function receives 2 identical arguments as input returns TRUE', function(){
+      var result = scope.sum(4,4);
+      expect(result).toBeTruthy();
+    });
+
+    it('If function receives 2 different arguments as input returns FALSE', function(){
+      var result = scope.sum(4,8);
+      expect(result).toBeFalsy();
+    });
+  });
+
+  describe('populate', function(){
+
+    beforeEach(inject(function () {
+      spyOn(scope, 'format').and.callThrough();
+      scope.populate();
+    }));
+
+    it('for each item in items-collection, it calls format function', function(){
+      // spyOn(scope, 'format').and.callThrough();
+      // scope.populate();
+
+      //expect(scope.format).toHaveBeenCalled();
+
+      expect(scope.format).toHaveBeenCalledTimes(scope.items.length);
+    });
 
   });
 
+  describe('format', function(){
 
+    var fake_item = {
+      'name': 'fake_name',
+      'username' : 'fake_user',
+      'email' : 'fake_emial'
+    };
 
+    it('functions updates the property \'email\'', function(){
+      scope.format(fake_item);
+      expect(fake_item.email).toBe('something@gmail.com');
+    });
+
+  });
 
 });
